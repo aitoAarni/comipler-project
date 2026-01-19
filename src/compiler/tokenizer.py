4f50c941e13e8b0c91711d544ce0c8f74b43f94c
aitoAarni <aarni.rechardt@gmail.com>
2026-01-19 19:26:39 +0100

    refactor

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,8 +1,7 @@
 import re
 
 def tokenizer(source_code: str="") -> list[str]:
-    r = re.compile(r"[1-9]*")
+    r = re.compile(r"[1-9]+")
 
-    unfiltered_tokens: list[str] = r.findall(source_code)
-    tokens = list(filter(bool, unfiltered_tokens))
+    tokens: list[str] = r.findall(source_code)
     return tokens
