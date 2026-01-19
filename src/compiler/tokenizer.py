4a8babe172854498d132db144d05ca0d27b0e8fe
aitoAarni <aarni.rechardt@gmail.com>
2026-01-19 21:31:34 +0100

    Task 1 done and refactoring

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,19 +1,28 @@
 import re
 
-def tokenizer(source_code: str="") -> list[str]:
+def add_whitespace_match(regex: str) -> str:
     white_space = r"[\n|\t| ]"
-    identifier = r"[a-zA-Z|_][a-zA-Z|_|0-9]*"
-    positive_integer = r"[0-9]+"
-    r = re.compile(rf"{white_space}{identifier}{white_space}|"
-                   rf"{white_space}{positive_integer}{white_space}")
+    return white_space + regex + white_space
 
+def get_regex_for_token(regex: str) -> str:
+    tokenizer_regexes = {
+        "identifier": r"[a-zA-Z|_][a-zA-Z|_|0-9]*",
+        "positive_integer" : r"[0-9]+",
+    }
+    return add_whitespace_match(tokenizer_regexes[regex])
+    
+
+def tokenizer(source_code: str="") -> list[str]:
+    identifier = get_regex_for_token("identifier")
+    positive_integer = get_regex_for_token("positive_integer")
+    r = re.compile(rf"{identifier}|{positive_integer}")
     source_code_spaced =  " " + source_code + " "
     previous_match_end = 0
     tokens: list[str] = []
     while (True):
         match = r.search(source_code_spaced, previous_match_end)
         if (not match): break
-        print("match:", match[0])
+        print("match:", match)
 
         previous_match_end = match.end() - 1
         tokens.append(match[0].strip())
