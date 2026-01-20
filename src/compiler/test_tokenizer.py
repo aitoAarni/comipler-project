c136ce719ce04a2d8227052a2c061dff4573c3ab
aitoAarni <aarni.rechardt@gmail.com>
2026-01-20 21:34:28 +0200

    Task2 done

--- src/compiler/test_tokenizer.py
+++ src/compiler/test_tokenizer.py
@@ -1,5 +1,15 @@
 import pytest
-from .tokenizer import tokenizer
+from .tokenizer import tokenizer, Token, SourceLocation
+
+def create_tokens(token_type: str, *values) -> list[Token]:
+    lis: list[Token] = []
+    for value in values:
+        location = SourceLocation(0, 0)
+        location._testing = True
+        lis.append(Token(
+            value if type(value) == str else str(value) , token_type, location
+        ))
+    return lis
 
 def test_empty_input() -> None:
     assert tokenizer() == []
@@ -8,12 +18,15 @@ def test_ignore_white_space() -> None:
     assert tokenizer("  \n\t") == []
 
 def test_tokenizes_positive_integers() -> None:
-    assert tokenizer("12 34\t56\n78   9") == ["12", "34", "56", "78", "9"]
+    correct_result = create_tokens(
+        "int_literal",
+        12, 34, 56, 78, 9
+    )
+    assert tokenizer("12 34\t56\n78   9") == correct_result
 
 def test_tokenizes_identifiers() -> None:
-    assert tokenizer("if \nwhile _var1\nOk_2") == [
-        "if", "while", "_var1", "Ok_2"
-        ]
+    correct_results = create_tokens("identifier", "if", "while", "_var1", "Ok_2")
+    assert tokenizer("if \nwhile _var1\nOk_2") == correct_results
 
 def test_throws_error_on_invalid_identifiers() -> None:
     with pytest.raises(Exception, match="Invalid syntax. Could not tokenize 2var"):
--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,43 +1,70 @@
 import re
+from dataclasses import dataclass, field
+
+@dataclass
+class SourceLocation:
+    line: int
+    column: int
+    _testing: bool = field(default=False, init=False, repr=False)
+
+    def __eq__(self, other):
+        if self._testing or other._testing: return True
+        return (self.line, self.column) == (other.line, other.column)
+
+@dataclass
+class Token:
+    text: str
+    type: str
+    location: SourceLocation
 
 
-def tokenizer(source_code: str="") -> list[str]:
+
+def tokenizer(source_code: str="") -> list[Token]:
     identifier = get_regex_for_token("identifier")
-    positive_integer = get_regex_for_token("positive_integer")
+    int_literal = get_regex_for_token("int_literal")
     white_space = get_regex_for_token("white_space")
 
     identifier_re = re.compile(identifier)
-    positive_integer_re = re.compile(positive_integer)
+    positive_integer_re = re.compile(int_literal)
     white_space_re = re.compile(white_space)
     regular_expressions = [identifier_re, positive_integer_re, white_space_re]
     previous_match_end = 0
     tokens: list[str] = []
     require_white_space_after = False
-
+    token_line = 1
     while (previous_match_end < len(source_code)):
         for regex in regular_expressions:
             match = regex.search(source_code, previous_match_end)
             if match and match.start() == previous_match_end:
                 previous_match_end = match.end()
                 if regex == white_space_re:
+                    token_line += match[0].count("\n")
                     require_white_space_after = False
                 elif require_white_space_after:
-                    raise Exception(f"Invalid syntax. Could not tokenize {source_code}")
+                    raise Exception(
+                        f"Invalid syntax. Could not tokenize {source_code}"
+                        )
+
                 elif regex == identifier_re:
-                    tokens.append(match[0])
+                    token = Token(match[0], "identifier",
+                                    SourceLocation(token_line, match.end()+1))
+                    tokens.append(token)
                 elif regex == positive_integer_re:
                     require_white_space_after = True
-                    tokens.append(match[0])
+                    token = Token(match[0], "int_literal",
+                                    SourceLocation(token_line, match.end()+1))
+                    tokens.append(token)
                 break
         else:
             raise Exception(f"Invalid syntax. Could not tokenize {source_code}")
 
     return tokens
 
+
 def get_regex_for_token(regex: str) -> str:
     tokenizer_regexes = {
         "identifier": r"[a-zA-Z|_][a-zA-Z|_|0-9]*",
-        "positive_integer" : r"[0-9]+",
+        "int_literal" : r"[0-9]+",
         "white_space" : r"[\n|\t| ]+"
     }
     return tokenizer_regexes[regex]
