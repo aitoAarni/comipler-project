9e34d039f97ed5432589e14356afbb80d06780db
aitoAarni <aarni.rechardt@gmail.com>
2026-02-26 20:08:42 +0200

    fix typing and formatting

--- src/compiler/test_type_checker.py
+++ src/compiler/test_type_checker.py
@@ -19,59 +19,59 @@ def create_ast(text: str) -> ast.Expression:
     return abstract_syntax_tree
 
 
-def test_int_type(type_table: SymTab):
+def test_int_type(type_table: SymTab) -> None:
     parsed = create_ast("328")
     assert typecheck(parsed, type_table) == Int
 
 
-def test_bool_type(type_table: SymTab):
+def test_bool_type(type_table: SymTab) -> None:
     parsed = create_ast("false")
     assert typecheck(parsed, type_table) == Bool
 
 
-def test_variable_declaration(type_table: SymTab):
+def test_variable_declaration(type_table: SymTab) -> None:
     parsed = create_ast("var x = 4")
     typecheck(parsed, type_table)
     assert type_table.get_symbol("x") == Int
 
 
-def test_block_statement_type(type_table: SymTab):
+def test_block_statement_type(type_table: SymTab) -> None:
     parsed = create_ast("32; {32; true}")
     assert typecheck(parsed, type_table) == Bool
 
 
-def test_variable_type(type_table: SymTab):
+def test_variable_type(type_table: SymTab) -> None:
     parsed = create_ast("var x = false; 2; x")
     assert typecheck(parsed, type_table) == Bool
 
 
-def test_assignmnent_type(type_table: SymTab):
+def test_assignmnent_type(type_table: SymTab) -> None:
     parsed = create_ast("var x = 3; x = 45")
     assert typecheck(parsed, type_table) == Unit
 
 
-def test_wrong_scope_throws(type_table: SymTab):
+def test_wrong_scope_throws(type_table: SymTab) -> None:
     parsed = create_ast("{var x = 2}; x * 2")
     with pytest.raises(Exception, match="There is no variable 'x' declared"):
         typecheck(parsed, type_table)
 
 
-def test_nested_variable_works(type_table: SymTab):
+def test_nested_variable_works(type_table: SymTab) -> None:
     parsed = create_ast("var x = 2; {var b = x; x / 2; {x + b + 2}}")
     assert typecheck(parsed, type_table) == Int
 
 
-def test_binary_opeartor_types(type_table: SymTab):
+def test_binary_opeartor_types(type_table: SymTab) -> None:
     lt = create_ast("(1 < 3) == false")
     assert typecheck(lt, type_table) == Bool
 
 
-def test_function_call_type(type_table: SymTab):
+def test_function_call_type(type_table: SymTab) -> None:
     parsed = create_ast("print_int(2)")
     assert typecheck(parsed, type_table) == Unit
 
 
-def test_function_call_throws_w_too_many_args(type_table: SymTab):
+def test_function_call_throws_w_too_many_args(type_table: SymTab) -> None:
     with pytest.raises(
         Exception,
         match=r"Error: function print_int takes 1 argument\(s\), but 2 were given.",
@@ -79,7 +79,7 @@ def test_function_call_throws_w_too_many_args(type_table: SymTab):
         typecheck(create_ast("print_int(2, 2)"), type_table)
 
 
-def test_function_call_throws_w_wrong_args(type_table: SymTab):
+def test_function_call_throws_w_wrong_args(type_table: SymTab) -> None:
     with pytest.raises(
         Exception,
         match=r"Error: function print_int expected paremater type Int, but got instead Bool: Literal\(location=\(1, 11\), value=True\).",
@@ -87,12 +87,12 @@ def test_function_call_throws_w_wrong_args(type_table: SymTab):
         typecheck(create_ast("print_int(true)"), type_table)
 
 
-def test_unary_operation_type(type_table: SymTab):
+def test_unary_operation_type(type_table: SymTab) -> None:
     parsed = create_ast("-1")
     assert typecheck(parsed, type_table) == Int
 
 
-def test_unary_operation_type(type_table: SymTab):
+def test_unary_operation_throws_w_wrong_type(type_table: SymTab) -> None:
     parsed = create_ast("not 20")
     with pytest.raises(
         Exception,
@@ -101,22 +101,23 @@ def test_unary_operation_type(type_table: SymTab):
         typecheck(parsed, type_table)
 
 
-def test_ternary_operation_type(type_table: SymTab):
+def test_ternary_operation_type(type_table: SymTab) -> None:
     parsed = create_ast("if 2 != 4 then 3 else 4")
     assert typecheck(parsed, type_table) == Int
 
 
-def test_ternary_operation_with_one_branch(type_table: SymTab):
+def test_ternary_operation_with_one_branch(type_table: SymTab) -> None:
     parsed = create_ast("if 2 != 4 then false")
     assert typecheck(parsed, type_table) == Bool
 
 
-def test_ternary_operator_with_1_branch(type_table: SymTab):
+def test_ternary_operator_with_1_branch(type_table: SymTab) -> None:
     parsed = create_ast("if 2 != 4 then false")
     assert typecheck(parsed, type_table) == Bool
 
 
-def test_ternary_opeartor_thorws_with_different_return_types(type_table: SymTab):
+def test_ternary_opeartor_thorws_with_different_return_types(
+        type_table: SymTab) -> None:
     parsed = create_ast("var x = 1; if 2 != 4 then false else x = 3")
     with pytest.raises(
         Exception,
@@ -124,6 +125,7 @@ def test_ternary_opeartor_thorws_with_different_return_types(type_table: SymTab)
     ):
         typecheck(parsed, type_table)
 
-def test_while_statement_type(type_table: SymTab):
+
+def test_while_statement_type(type_table: SymTab) -> None:
     parsed = create_ast("while true do {12 % 6}")
     assert typecheck(parsed, type_table) == Int
