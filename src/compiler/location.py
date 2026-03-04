457e0685d14de8776e1854925b59e0d4438fde7d
aitoAarni <aarni.rechardt@gmail.com>
2026-03-04 12:43:36 +0200

    fix type annotiations

--- src/compiler/location.py
+++ src/compiler/location.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Self
 from dataclasses import dataclass, field
 
@@ -8,18 +9,19 @@ class Location:
     column: int | None = None
     _testing: bool = field(default=False, init=False, repr=False)
 
-    def new(self) -> Self:
+    def new(self) -> Location:
         new_source_location = Location(self.line, self.column)
         new_source_location._testing = self._testing
         return new_source_location
 
-    def __eq__(self, other):
+    def __eq__(self: Self, other: object) -> bool:
+        if not isinstance(other, Location): return False
         if self._testing or other._testing:
             return True
         return (self.line, self.column) == (other.line, other.column)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"({self.line}, {self.column})"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"({self.line}, {self.column})"
