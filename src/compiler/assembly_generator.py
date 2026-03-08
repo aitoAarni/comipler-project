d84f33213739ef929b8eea9c6e2407d607c3aab4
aitoAarni <aarni.rechardt@gmail.com>
2026-03-08 16:00:02 +0200

    add unary not to assembly code generation

--- src/compiler/assembly_generator.py
+++ src/compiler/assembly_generator.py
@@ -22,17 +22,17 @@ class Locals:
         return self._stack_used
 
 
-def generate_assembly(instructions: list[ir.Instruction]) -> str:
+def generate_assembly(instructions: list[ir.Instruction], local_vars: list[ir.IRVar]) -> str:
     lines = []
     def emit(line: str) -> None: lines.append(line)
 
     locals = Locals(
-        variables=get_all_ir_variables(instructions)
+        local_vars
     )
 
     # ... Emit initial declarations and stack setup here ...
     emit("pushq %rbp")
-    emit("moveq %rsp, %rbp")
+    emit("movq %rsp, %rbp")
     emit(f"subq  ${locals._stack_used + 8}, %rsp")
 
     for insn in instructions:
@@ -44,6 +44,7 @@ def generate_assembly(instructions: list[ir.Instruction]) -> str:
                 # This makes GDB backtraces look nicer too:
                 # https://stackoverflow.com/a/26065570/965979
                 emit(f'.L{insn.name}:')
+                continue
             case ir.LoadIntConst():
                 if -2**31 <= insn.value < 2**31:
                     emit(f'movq ${insn.value}, {locals.get_ref(insn.dest)}')
@@ -54,33 +55,43 @@ def generate_assembly(instructions: list[ir.Instruction]) -> str:
                     # not a memory location, so we use %rax
                     # as a temporary.
                     emit(f'movabsq ${insn.value}, %rax')
+                continue
 
             case ir.LoadBoolConst():
                 emit(
                     f"movq $"
                     f"{1 if insn.value else 0} "
                     f"{locals.get_ref(insn.dest)}")
-
+                continue
             case ir.Copy():
                 emit(f"movq {locals.get_ref(insn.source)}, %rax")
                 emit(f"movq %rax, {locals.get_ref(insn.dest)}")
-
+                continue
+                        
             case ir.CondJump():
                 emit(f"cmpq $0, {locals.get_ref(insn.cond)}")
                 emit(f"jne .L{insn.then_label.name}")
                 emit(f"jmp .L{insn.else_label.name}")
+                continue
             case ir.Jump():
                 emit(f'jmp .L{insn.label.name}')
+                continue
             case ir.Call():
                 args = [locals.get_ref(arg) for arg in insn.args]
                 result = locals.get_ref(insn.dest)
                 match insn.fun.name:
                     case "unary_-":
-                        emit(f"moveq {args[0]}, %rax")                        
+                        emit(f"movq {args[0]}, %rax")                        
                         emit(f"negq %rax")                        
-                        emit(f"moveq %rax, {result}")                        
+                        emit(f"movq %rax, {result}")                        
+                        continue
+                        
+                    case "unary_not":
+                        emit(f"movq {args[0]}, %rax")
+                        emit(f"xorq $1, %rax")
+                        emit(f"movq %rax, {result}")
 
-    emit(f"moveq %rbp, %rsp")
+    emit(f"movq %rbp, %rsp")
     emit(f"popq %rbp")
     emit(f"ret")
     return lines
@@ -92,16 +103,15 @@ if __name__ == "__main__":
     from compiler.parser import parse
     from compiler.type_checker import typecheck
     from compiler.utils import GLOBAL_VARS
-    from compiler.ir_generator import generate_ir
+    from compiler.ir_generator import IrGenerator
     from compiler.utils import create_top_level_type_symbol_table
 
-    code = "-1"
+    code = "not true"
     tokens = tokenizer(code)
     parsed = parse(tokens)
     type_table = create_top_level_type_symbol_table()
     typecheck(parsed, type_table)
-    intermediate_representation = generate_ir(
-        set(GLOBAL_VARS), parsed)
-    
-    for line in generate_assembly(intermediate_representation):
+    ir_gen = IrGenerator(set(GLOBAL_VARS), parsed)
+    intermediate_representation = ir_gen.generate_ir()   
+    for line in generate_assembly(intermediate_representation, ir_gen.get_locals()):
         print(line)
